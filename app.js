d1a3dadf8d4103a3cdf6c7a3440d032fcd284795
Colin Beuttler <beutt102@mail.chapman.edu>
2021-11-02 13:46:19 -0700

    update

--- app.js
+++ app.js
@@ -5,34 +5,45 @@ const playerEl = document.querySelector(".boxclass");
 const inputNameEl = document.querySelector(".form__input--playerName");
 const btnPlayerForm = document.querySelector(".btn--playerSub");
 
-// button Calls
-btnPlayerForm.addEventListener("click", createPlayer, renderPlayer);
+class PlayerAdd {
+  constructor() {
+    // button Calls
+    btnPlayerForm.addEventListener(
+      "click",
+      this._createPlayer,
+      this._renderPlayer
+    );
+  }
+  // button Functions
+  _createPlayer(e) {
+    let playerName = inputNameEl.value;
+    let playerObj = [];
 
-// button Functions
-function createPlayer(e) {
-  let playerName = inputNameEl.value;
-  let playerObj = [];
+    let currentPlayer = playerObj[0];
+    let player;
 
-  let currentPlayer = playerObj[0];
-  let player;
+    e.preventDefault();
 
-  e.preventDefault();
+    // if (inputNameEl.hasOwnProperty(playerName)) return alert("Enter valid Input");
 
-  // if (inputNameEl.hasOwnProperty(playerName)) return alert("Enter valid Input");
+    player = new Player(playerName);
+    console.log(player);
 
-  player = new Player(playerName);
-  console.log(player);
+    playerObj.push(player);
+    console.log(playerObj);
+    return playerObj;
+  }
 
-  playerObj.push(player);
-  console.log(playerObj);
-  return playerObj;
-}
-
-function renderPlayer(playerObj) {
-  let html = `<div class="boxclass">
+  _renderPlayer(playerObj) {
+    let html = `<div class="boxclass">
     <h1>${playerObj.player}</h1>
     <div class="playerDisc">
     </div>
   </div>`;
-  playerEl.insertAdjacentHTML("afterend", html);
+    playerEl.insertAdjacentHTML("afterend", html);
+  }
 }
+
+const PlayerApp = new PlayerAdd();
+
+export default PlayerApp;
