5c776421870b52cc0a3021c79ddd0e80611f7879
Colin Beuttler <beutt102@mail.chapman.edu>
2021-12-02 22:20:42 -0800

    classoptinoslect

--- script.js
+++ script.js
@@ -147,7 +147,7 @@ class GameApp {
     let opt = [moldEl, plasticEl, colorEl, weightEl];
     let el = document.createElement("option");
     el.textContent = opt;
-    el.value = opt;
+    el.classList.add("selectorOption");
     select.add(el);
   }
 
