a762e60f432a681facd934727bb082ce0bc9fd4d
Colin <beutt102@mail.chapman.edu>
2022-01-06 13:11:19 -0800

    popupJSCreate

---
+++ popup.js
@@ -0,0 +1,17 @@
+
+let newPlayerHtml =` <div class="newPlayer">
+<form class="newPlayerForm">
+  <input type="text" placeholder="New Player Name" class="form__input form__input--playerName"/>
+  <input type="button" value="Add New Player" class="btn btn--playerSub"></input>
+</form>
+</div>  `
+
+let newDiscHtml =`  <div class="newDisc">
+<form class="newDiscForm">
+  <input type="text" placeholder="Disc Mold" class="input--mold" id="discInput" />
+  <input type="text" placeholder="Disc Plastic" class="input--plastic" id="discInput"/>
+  <input type="text" placeholder="Disc Color" class="input--color" id="discInput"/>
+  <input type="text" placeholder="Disc Weight" class="input--weight" id="discInput"/>
+  <input type="button" value="Add Disc" class="btn btn--discSub"></input>
+</form>
+</div>`
