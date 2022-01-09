34e218bf4888ffc4ad79701bd6e3c17e8497ddcf
Colin <beutt102@mail.chapman.edu>
2022-01-09 13:48:12 -0800

    btnCSS

--- JS/popup.js
+++ JS/popup.js
@@ -1,12 +1,12 @@
 
-let newPlayerHtml =` <div class="newPlayer">
+let newPlayerHtml =`<div class="newPlayer">
 <form class="newPlayerForm">
   <input type="text" placeholder="New Player Name" class="form__input form__input--playerName"/>
   <input type="button" value="Add New Player" class="btn btn--playerSub"></input>
 </form>
-</div>  `
+</div>`
 
-let newDiscHtml =`  <div class="newDisc">
+let newDiscHtml =`<div class="newDisc">
 <form class="newDiscForm">
   <input type="text" placeholder="Disc Mold" class="input--mold" id="discInput" />
   <input type="text" placeholder="Disc Plastic" class="input--plastic" id="discInput"/>
