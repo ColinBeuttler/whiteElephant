742c1d351f796c5d82f18d01dd29c04a23955bd3
Colin <beutt102@mail.chapman.edu>
2022-01-11 10:52:29 -0800

    popupCSS

--- JS/popup.js
+++ JS/popup.js
@@ -1,3 +1,7 @@
+// Html elements
+const popUpEl = document.querySelector(".popUpWin")
+
+
 
 class Popup{
   constructor(){
@@ -6,32 +10,8 @@ class Popup{
 
   }
   _getPopup(){
-    let popupHtml =`
-<div class='popupWin'>
-  <div class="newPlayer">
-  <form class="newPlayerForm">
-    <input type="text" placeholder="New Player Name" class="form__input form__input--playerName"/>
-    <input type="button" value="Add New Player" class="btn btn--playerSub"></input>
-  </form>
-  </div>
 
-  <div class="newDisc">
-  <form class="newDiscForm">
-    <input type="text" placeholder="Disc Mold" class="input--mold" id="discInput" />
-    <input type="text" placeholder="Disc Plastic" class="input--plastic" id="discInput"/>
-    <input type="text" placeholder="Disc Color" class="input--color" id="discInput"/>
-    <input type="text" placeholder="Disc Weight" class="input--weight" id="discInput"/>
-    <input type="button" value="Add Disc" class="btn btn--discSub"></input>
-  </form>
-  </div>
-
-  <form>
-  <input type="button" value="Start Game" class="btn btn--gameStart"></input>
-  </form>
-
-</div>`
-
-body.insertAdjacentHTML("afterbegin", popupHtml);
+ popUpEl.add
 
   }
 
