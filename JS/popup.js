1d19c0252e3920dfe14dbfd711149924728ee454
Colin <beutt102@mail.chapman.edu>
2022-01-11 18:32:48 -0800

    cssPopUp

--- JS/popup.js
+++ JS/popup.js
@@ -1,5 +1,8 @@
 // Html elements
 const popUpEl = document.querySelector(".popUpWin")
+const gameStartEl = document.querySelector('.btn--startGame')
+
+// console.log(document.querySelector('.popUpWin'))
 
 
 
@@ -7,12 +10,19 @@ class Popup{
   constructor(){
     this._getPopup()
 
+    gameStartEl.addEventListener('click', this._startGame.bind(this))
+
 
   }
   _getPopup(){
 
- 
 
+  }
+
+  _startGame(){
+    console.log(true)
+    
+    popUpEl.style.display = "none";
   }
 
 
