b40fa2dadf250deed78561802fc615a26ef3c9c1
Colin <beutt102@mail.chapman.edu>
2022-01-27 12:11:43 -0800

    addGiftObj

---
+++ Public/JS/giftObj.js
@@ -0,0 +1,14 @@
+// gift Form elements for event listeners
+const inputGiftEl = document.querySelector(".input--gift");
+const btnGiftForm = document.querySelector(".btn--giftSub");
+const giftList = document.querySelector(".giftList");
+
+// Gift objects global variable
+let gifts = [];
+
+// new Gift object prototype
+class Gift {
+  constructor(gift) {
+    this.gift = gift;
+  }
+}
