88f687dae5f41942de41a06ad25d3c31b6f5c5ee
Colin <beutt102@mail.chapman.edu>
2022-01-28 17:38:34 -0800

    classGiftAdd

--- Public/JS/giftObj.js
+++ Public/JS/giftObj.js
@@ -2,6 +2,7 @@
 const inputGiftEl = document.querySelector(".input--gift");
 const btnGiftForm = document.querySelector(".btn--giftSub");
 const giftList = document.querySelector(".giftList");
+const inputGiftValue = document.querySelector('.input--giftValue')
 
 // Gift objects global variable
 let gifts = [];
@@ -12,4 +13,24 @@ class Gift {
     this.gift = gift;
     this.giftValue = giftValue
   }
+}
+
+class GiftAdd{
+    constructor(){
+        this._getLocalGifts();
+
+        btnGiftForm.addEventListener('click', this._createGift.bind(this))
+    }
+
+   _createGift(){
+       const giftName = inputGiftEl.value
+       const giftValue = inputGiftValue.value
+
+       let gift 
+
+   } 
+
+   _getLocalGifts(){
+
+   }
 }
